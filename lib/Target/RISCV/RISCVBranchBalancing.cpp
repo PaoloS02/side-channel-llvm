5a107966c13cafe6c6c0307dddc8646045cfdb23
Paolo Savini <paolo.savini@embecosm.com>
2018-07-09 16:23:41 +0100

    Reorganized the code

--- lib/Target/RISCV/RISCVBranchBalancing.cpp
+++ lib/Target/RISCV/RISCVBranchBalancing.cpp
@@ -13,71 +13,23 @@ using namespace llvm;
 
 namespace{
 
-	
-	
 	struct RISCVBranchBalancer : public MachineFunctionPass{
+	
 		static char ID;
 		RISCVBranchBalancer() : MachineFunctionPass(ID) {}
 		MachineDominatorTree *MDT;
 		
-		void getAnalysisUsage(AnalysisUsage &AU) const override {
+		/*void getAnalysisUsage(AnalysisUsage &AU) const override {
 			AU.addRequired<MachineDominatorTree>();
+			MachineFunctionPass::getAnalysisUsage(AU);
 		  }
+		*/
+		void EqualBlocks(MachineFunction& MF);
 		
-		
-		bool runOnMachineFunction(MachineFunction& MF) override {
-		
-			int InstrCount = 0;
-			int maxInstrPerBlock = 0;
-			const TargetInstrInfo &TII = *MF.getSubtarget().getInstrInfo();
-			
-			for(MachineBasicBlock& MB : MF) {
-				InstrCount = 0;
-				
-				for(MachineInstr& MI : MB) {
-					InstrCount ++;
-				}
-				errs() << InstrCount << "\t" << MB.getFullName() << "\n";
-				if(InstrCount > maxInstrPerBlock)
-					maxInstrPerBlock = InstrCount;
-				
-				
-			}
-			
-			
-			for(MachineBasicBlock& MB : MF){
-				InstrCount = 0;
-				
-				for(MachineInstr& MI : MB){
-					InstrCount ++;
-				}
-				MachineInstr& MI = MB.instr_front();
-				
-				for(; InstrCount < maxInstrPerBlock;) {
-					BuildMI(MB, MI, MI.getDebugLoc(), TII.get(RISCV::ADDI))		//RISC-V NOOP OPERATION: ADDI $X0, $X0, 0
-						.addReg(RISCV::X0)
-						.addReg(RISCV::X0)
-						.addImm(0);
-					InstrCount++;
-				}
-			}
-			
-			InstrCount = 0;
-			for(MachineBasicBlock& MB : MF) {
-				for(MachineInstr& MI : MB) {
-					InstrCount ++;
-				}
-				errs() << InstrCount << "\t" << MB.getFullName() << "\n";
-				
-				InstrCount = 0;
-			}
-			
-			return true;
-		}
+		bool runOnMachineFunction(MachineFunction& MF) override ;
 		
 	};
-	
-	
+		
 }
 
 
@@ -85,6 +37,64 @@ char RISCVBranchBalancer::ID = 0;
 
 INITIALIZE_PASS(RISCVBranchBalancer, "riscv-block-instruction-counter",
                 RISCV_BRANCH_BALANCER, false, false)
+//INITIALIZE_PASS_DEPENDENCY(MachineDominatorTree)
+
+void RISCVBranchBalancer::EqualBlocks(MachineFunction& MF) {
+	int InstrCount = 0;
+	int maxInstrPerBlock = 0;
+	const TargetInstrInfo &TII = *MF.getSubtarget().getInstrInfo();
+	
+	for(MachineBasicBlock& MB : MF) {
+		InstrCount = 0;
+		
+		for(MachineInstr& MI : MB) {
+			InstrCount ++;
+		}
+		errs() << InstrCount << "\t" << MB.getFullName() << "\n";
+		if(InstrCount > maxInstrPerBlock)
+			maxInstrPerBlock = InstrCount;
+		
+		}
+			
+			
+	for(MachineBasicBlock& MB : MF){
+		InstrCount = 0;
+		
+		for(MachineInstr& MI : MB){
+			InstrCount ++;
+		}
+		MachineInstr& MI = MB.instr_front();
+		
+		for(; InstrCount < maxInstrPerBlock;) {
+			BuildMI(MB, MI, MI.getDebugLoc(), TII.get(RISCV::ADDI))		//RISC-V NOOP OPERATION: ADDI $X0, $X0, 0
+				.addReg(RISCV::X0)
+				.addReg(RISCV::X0)
+				.addImm(0);
+			InstrCount++;
+		}
+	}
+			
+	InstrCount = 0;
+	for(MachineBasicBlock& MB : MF) {
+		for(MachineInstr& MI : MB) {
+			InstrCount ++;
+		}
+		errs() << InstrCount << "\t" << MB.getFullName() << "\n";
+		
+		InstrCount = 0;
+	}	
+}
+
+
+bool RISCVBranchBalancer::runOnMachineFunction(MachineFunction& MF) {
+		
+			EqualBlocks(MF);
+			
+			return true;
+}
+
+
+
 
 FunctionPass *llvm::createRISCVBranchBalancerPass() {
   return new RISCVBranchBalancer();
