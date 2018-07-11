f7759e0b4bec07625a6b236d874e4aac1ad0ce05
Paolo Savini <paolo.savini@embecosm.com>
2018-07-11 18:21:50 +0100

    fixed bugged creation of dummy block. The replacement of the old successor of the parent block with the dummy block caused an unforseen iteration on the same block. Once the jump instruction from the parent block to the dummy is set the old successor is simply removed from the set of the successor of the parent block

--- lib/Target/RISCV/RISCVBranchBalancing.cpp
+++ lib/Target/RISCV/RISCVBranchBalancing.cpp
@@ -63,6 +63,8 @@ void RISCVBranchBalancer::findDomTreeLeaves(MachineFunction& MF, MachineDominato
 	unsigned int maxInstr = 0;
 	unsigned int instrCount = 0;
 	
+	bool waitForBranch = false;
+	
 	for(MachineBasicBlock& MBB : MF) {
 	
 		/*for(MachineInstr &MTerm : MBB.terminators()){
@@ -71,26 +73,56 @@ void RISCVBranchBalancer::findDomTreeLeaves(MachineFunction& MF, MachineDominato
 		}*/
 		costToLeaf = maxInstr = instrCount = 0;
 		
+		/*Finding the leaves: nodes with no children in the dominator 
+		  tree and just one predecessor in the control flow graph.*/
 		if((MDTREF.getNode(&MBB)->getNumChildren() == 0) && (MBB.pred_size() == 1)){ //invalidating multiple entry points, for now
 		
-			struct CostFromMBBToLeaf NewCostFromMBBToLeaf;
 			
-			for(CostFromMBBToLeaf CL : CostsFromMBBToLeaves){
-				if(CL.MBB == &MBB){
-					costToLeaf = CL.cost;
-					errs() << "FOUND:  " << CL.MBB->getParent()->getName() << "\n";
-				}
-			}
 			
+			/*Balancing operations: to be performed by considering all 
+			  concurrent children. If the node has more predecessors in
+			  the CFG such balancing must be made on all the predecerrors 
+			  together or on each predecessor separatelly. According to
+			  that the max number of instruction will need to be reset
+			  to 0 at each iteration*/
 			for(MachineBasicBlock *pred : MBB.predecessors()){
 				//maxInstr = instrCount = 0; //what when we have multiple entry points?
+			errs() << "BEGINNING  " << MBB.getParent()->getName() << "  " << MBB.getName() << "\n\tcost: " << costToLeaf << "\n\tmax: " << maxInstr << "\n\n";
+				waitForBranch = false;
+				costToLeaf = maxInstr = instrCount = 0;
 				
+				for(MachineBasicBlock *succ : pred->successors()){
+					if(MDTREF.getNode(succ)->getNumChildren() != 0)
+						waitForBranch = true;
+				}
+				
+				if(!waitForBranch) {
+				
+				
+				struct CostFromMBBToLeaf NewCostFromMBBToLeaf;
+		
+				/*Searching previously computed costs. If this leaf 
+				  is original it will have no previous cost*/
+			
+				for(CostFromMBBToLeaf CL : CostsFromMBBToLeaves){
+					if(CL.MBB == &MBB){
+						costToLeaf = CL.cost;
+						errs() << "FOUND:  " << CL.MBB->getParent()->getName() << "  " << MBB.getName() << "\n";
+					}
+				}
+				
+				/*Searching the maximum number of instructions among the 'brothers'*/
 				for(MachineBasicBlock *succ : pred->successors()){
 					if((succ->size() > maxInstr) && (!MBB.isSuccessor(succ))){
 						maxInstr = succ->size();
 					}
 				}
 				
+				/*Searching for direct paths between the predecessor 
+				  and the successor of the current block, if any, there's 
+				  a chance to skip the current block according to some 
+				  conditions. Such chance must be eliminated by interleaving 
+				  a dummy block equal in size*/
 				for(MachineBasicBlock *succ : pred->successors()){
 					if(MBB.isSuccessor(succ)) {
 						MachineBasicBlock *dummyMBB = MF.CreateMachineBasicBlock(MBB.getBasicBlock());
@@ -103,6 +135,7 @@ void RISCVBranchBalancer::findDomTreeLeaves(MachineFunction& MF, MachineDominato
 						
 						MachineInstr& MI = pred->back();
 						
+						/*The cost of previously processed nested nodes is considered too (costToLeaf)*/
 						for(instrCount = dummyMBB->size(); instrCount < maxInstr+costToLeaf-1;) {
 							BuildMI(*dummyMBB, dummyMBB->end(), MI.getDebugLoc(), TII.get(RISCV::ADDI))
 							.addReg(RISCV::X0)
@@ -110,11 +143,14 @@ void RISCVBranchBalancer::findDomTreeLeaves(MachineFunction& MF, MachineDominato
 							.addImm(0);			//RISC-V NOOP OPERATION: ADDI $X0, $X0, 0
 							instrCount++;
 						}
+						
+						/*Setting the links with the new block*/
 						BuildMI(*dummyMBB, dummyMBB->end(), MI.getDebugLoc(), TII.get(RISCV::JAL))
 						.addReg(RISCV::X0)
 						.addMBB(succ);
 						
-						pred->replaceSuccessor(succ, dummyMBB);
+						//pred->replaceSuccessor(succ, dummyMBB);
+						pred->removeSuccessor(succ);
 						
 						for(MachineInstr &MTerm : pred->terminators()){
 							if(MTerm.isBranch()){
@@ -127,6 +163,7 @@ void RISCVBranchBalancer::findDomTreeLeaves(MachineFunction& MF, MachineDominato
 					}
 				}
 				
+				/*Balancing all the brothers, also the dummy ones*/
 				for(MachineBasicBlock *succ : pred->successors()){
 					MachineInstr& MI = succ->instr_front();
 					
@@ -138,10 +175,15 @@ void RISCVBranchBalancer::findDomTreeLeaves(MachineFunction& MF, MachineDominato
 					}
 				}
 				
+				/*Keeping record of the costs of the leaves*/
 				NewCostFromMBBToLeaf.MBB = pred;
 				NewCostFromMBBToLeaf.cost = maxInstr + costToLeaf;
 				CostsFromMBBToLeaves.push_back(NewCostFromMBBToLeaf);
-				errs() << MBB.getParent()->getName() << "  " << MBB.getName() << "\n\tcost: " << costToLeaf << "\n\tmax: " << maxInstr << "\n\n";	
+				errs() << MBB.getParent()->getName() << "  " << MBB.getName() << "\n\tcost: " << costToLeaf << "\n\tmax: " << maxInstr << "\n\n";
+			
+				} //waitForBranch
+			
+			
 			}
 			
 		}
